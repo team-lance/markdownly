844dfbf86f95b9235d2332f258bd857f53172898
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-02-27 13:25:37 -0800

    selector tests passes

---
+++ src/selectors/markdownSelectors.test.js
@@ -0,0 +1,21 @@
+import { getMarkdown } from './markdownSelectors';
+
+describe('selector tests', () => {
+  it('test the get single markdown selector', () => {
+
+    const state = {
+      markdown: 'some string'
+    };
+    const markdown = getMarkdown(state);
+
+    expect(markdown).toEqual('some string');
+  });
+
+  it('it will return an empty state if state is empty', () => {
+    const state = {
+      markdown: ''
+    };
+    const markdown = getMarkdown(state);
+    expect(markdown).toEqual('');
+  });
+});
