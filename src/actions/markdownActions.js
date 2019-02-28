bf24de2e1ff37743d4e6fcb085ba43267ee3718b
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-27 16:52:44 -0800

    separate markdownList action into own file

--- src/actions/markdownActions.js
+++ src/actions/markdownActions.js
@@ -3,9 +3,3 @@ export const updateMarkdown = text => ({
   type: UPDATE_MARKDOWN,
   payload: text
 });
-
-export const CREATE_MARKDOWN = 'CREATE_MARKDOWN';
-export const createMarkdown = markdown => ({
-  type: CREATE_MARKDOWN,
-  payload: markdown
-});
