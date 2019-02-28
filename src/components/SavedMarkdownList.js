0705f43a53e004d94fc8459ddd94d7875718c1ac
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-02-28 14:43:03 -0800

    Fixed some white spacing issues based on reviewer`s sugestions for consistency cross the applciation

--- src/components/SavedMarkdownList.js
+++ src/components/SavedMarkdownList.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 export function MarkdownTitles({ titles }) {
-
   const listOfTitles = titles.map((title, i)  => {
-    return <li key={i}> {title}</li>;
+    return <li key={i}>{title}</li>;
   });
 
   return (
