03c79d317dd69c0dccedaea785fcb76d5a0672f0
Cari Palazzolo <42423180+caripizza@users.noreply.github.com>
2019-02-28 13:14:32 -0800

    Added favicon to repo (#15)
    * added favicon to repo and webackconfig
    
    * remove unused code in webackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 
+// eslint-disable-next-line
 module.exports = {
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
@@ -10,7 +11,10 @@ module.exports = {
     port: 7890
   },
   plugins: [
-    new HtmlPlugin({ template: './src/index.html' }),
+    new HtmlPlugin({
+      template: './src/index.html',
+      favicon: './src/assets/favicon.ico'
+    }),
     new CleanPlugin('./dist')
   ],
   module: {
