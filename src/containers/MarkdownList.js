c4d9d0c5e1a2fcdc2466c18d3b793e5fb3f1c44d
Lance Owen Merrill <40847460+Zilula@users.noreply.github.com>
2019-02-28 15:31:06 -0800

    Markdown list container (#17)
    *  Created a component that would render a list of available markdown files from our applications store
    
    * Created a selector that gets all the titles from our list of markdown files and a test that tests that it actually returns a list of titles from out store
    
    * Moved title selector from its own file into our other file that will hold all of the markdown selectors regarding lists and moved the test accordingly
    
    *  Created a container component that makes a call to state to grab a list of markdown file`s titles and uses a presentational component to render that list
    
    * Fixed some white spacing issues based on reviewer`s sugestions for consistency cross the applciation
    
    *  Fixed whitespacing issues that were over looked during the previous commit
    
    * Update src/components/SavedMarkdownList.js
    
    Co-Authored-By: Zilula <40847460+Zilula@users.noreply.github.com>

--- src/containers/MarkdownList.js
+++ src/containers/MarkdownList.js
@@ -1,2 +1,32 @@
-import React from 'react';
-import { getTitles } from '../selectors/getTitlesSelector';
+import React, { PureComponent } from 'react';
+import { getTitles } from '../selectors/markdownListSelectors';
+import { MarkdownTitles } from '../components/SavedMarkdownList';
+import store from '../store';
+
+export default class MarkdownList extends PureComponent {
+    state = {
+      markdownTitles: []
+    };
+
+    componentDidMount() {
+      this.unsubscribe = store.subscribe(() => {
+        const state = store.getState();
+        const markdownTitles = getTitles(state);
+        this.setState({ markdownTitles });
+      });
+    }
+    componentWillUnmount() {
+      if(this.unsubscribe) {
+        this.unsubscribe();
+      }
+    }
+
+    render() {
+      const { markdownTitles } = this.state;
+      return (
+        <MarkdownTitles
+          titles={markdownTitles}
+        />
+      );
+    }
+}
